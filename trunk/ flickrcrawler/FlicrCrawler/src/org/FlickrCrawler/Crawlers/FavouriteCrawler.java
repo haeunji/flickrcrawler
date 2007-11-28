ba998a7322eacab3765626bdb1e8306dfe53c938
rogermzy <rogermzy@91f5a166-af3e-0410-b530-b5dbff717f50>
2007-11-28 12:19:12 +0000

    haven't finished favourite

--- trunk/ flickrcrawler/FlicrCrawler/src/org/FlickrCrawler/Crawlers/FavouriteCrawler.java
+++ trunk/ flickrcrawler/FlicrCrawler/src/org/FlickrCrawler/Crawlers/FavouriteCrawler.java
@@ -1,5 +1,31 @@
 package org.FlickrCrawler.Crawlers;
 
+import org.FlickrCrawler.database.Database;
+
+import com.aetrion.flickr.Flickr;
+import com.aetrion.flickr.favorites.FavoritesInterface;
+
 public class FavouriteCrawler {
+	
+	FavoritesInterface favoritesinterface;
+	Database db;
+	
+	public FavouriteCrawler(Flickr f){
+		
+		favoritesinterface = f.getFavoritesInterface();
+		db = new Database();
+		
+	}
+	
+	/**
+	 * Takes the UserId and get a list of fav pics by this userid then store the in
+	 * the database
+	 * 
+	 * 
+	 * @param UserId
+	 */
+	public void Crawl(String UserId){
+		favoritesinterface.getPublicList(UserId, arg1, arg2, arg3)
+	}
 
 }
